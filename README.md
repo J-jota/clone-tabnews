71ebcfde17714f5faf65b9fdb84cf238ad7061b0
J-jota <josemarianodonascimento@hotmail.com>
2025-09-02 02:25:32 +0000

    adiciona scripts `lint:check` e `lint:fix`

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # clone-tabnews
+
 Repositório de estudo para implementação de um projeto End 2 End.
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,13 @@
 function Home() {
-    return <h1>Olá Rosinete Sabino, como vai?</h1>
+  return <h1>Olá Rosinete Sabino, como vai?</h1>;
+}
+
+function teste() {
+  console.log("teste");
+}
+
+function teste2() {
+  console.log("teste");
 }
 
 export default Home;
