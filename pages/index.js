0cbb78cd370d728f7a173de4d5f224b6507e97f9
J-jota <josemarianodonascimento@hotmail.com>
2025-09-11 02:29:01 +0000

    instalar o test runner `jest` e configurar scripts de teste

--- pages/index.js
+++ pages/index.js
@@ -2,12 +2,4 @@ function Home() {
   return <h1>Olá Rosinete Sabino, como vai?</h1>;
 }
 
-function teste() {
-  console.log("teste");
-}
-
-function teste2() {
-  console.log("teste");
-}
-
 export default Home;
